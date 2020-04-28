ef0890400f64a3fb850947d04b1fc02572941c5c
mizhiyangg <61826240+mizhiyangg@users.noreply.github.com>
2020-04-28 13:47:08 +0800

    四个主页面的布局完成，交友页书本详情页连服务器数据的时候再写

--- app/src/main/java/cn/itcast/ppx/FriFragment.java
+++ app/src/main/java/cn/itcast/ppx/FriFragment.java
@@ -2,11 +2,25 @@ package cn.itcast.ppx;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FriFragment extends Fragment {
+
+    private TextView mTime;
+
+    private ListView mListView;
 
     public FriFragment(){
 
@@ -15,6 +29,87 @@ public class FriFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fri, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_fri,container,false);
+
+        mTime=view.findViewById(R.id.tv_time);
+        new TimeThread().start();
+
+        mListView=(ListView) view.findViewById(R.id.lv_fri);
+
+        MyAdapter myAdapter=new MyAdapter();
+
+        mListView.setAdapter(myAdapter);
+
+        return view;
     }
+
+    public class TimeThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            do{
+                try {
+                    Thread.sleep(1000);
+                    Message msg = new Message();
+                    msg.what = 1;
+                    handler.sendMessage(msg);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }while (true);
+        }
+    }
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what){
+                case 1:
+                    mTime.setText(new SimpleDateFormat("HH:mm:ss").format(new Date(System.currentTimeMillis())));
+                    break;
+            }
+            return false;
+        }
+    });
+
+
+    private class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 7;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView=View.inflate(getContext(),R.layout.fri_list_item,null);
+                holder=new ViewHolder();
+                holder.mTitle=convertView.findViewById(R.id.tv_title);
+                holder.mImageView=convertView.findViewById(R.id.iv_icon);
+                convertView.setTag(holder);
+            }else {
+                holder=(ViewHolder) convertView.getTag();
+            }
+            return convertView;
+        }
+    }
+
+    class ViewHolder{
+        TextView mTitle;
+        ImageView mImageView;
+    }
+
 }
--- app/src/main/java/cn/itcast/ppx/MineFragment.java
+++ app/src/main/java/cn/itcast/ppx/MineFragment.java
@@ -1,12 +1,20 @@
 package cn.itcast.ppx;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MineFragment extends Fragment {
+
+    private ListView mListView;
 
     public MineFragment(){
 
@@ -15,6 +23,81 @@ public class MineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mine, container, false);
+        View view=inflater.inflate(R.layout.fragment_mine, container, false);
+
+        mListView=(ListView) view.findViewById(R.id.lv_list);
+
+        MyAdapter myAdapter=new MyAdapter();
+
+        mListView.setAdapter(myAdapter);
+
+        setListViewHeightBasedOnChildren(mListView);
+
+        return view;
     }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    private class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView=View.inflate(getContext(),R.layout.dongtai_list_item,null);
+                holder=new ViewHolder();
+                convertView.setTag(holder);
+            }else {
+                holder=(ViewHolder) convertView.getTag();
+            }
+            return convertView;
+        }
+    }
+
+    class ViewHolder{
+        TextView mTitle;
+        TextView mContent;
+    }
+
+    public void setListViewHeightBasedOnChildren(ListView listView) {
+        // 获取ListView对应的Adapter
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+        int totalHeight = 0;
+        for (int i = 0, len = listAdapter.getCount(); i < len; i++) {
+            // listAdapter.getCount()返回数据项的数目
+            View listItem = listAdapter.getView(i, null, listView);
+            // 计算子项View 的宽高
+            listItem.measure(0, 0);
+            // 统计所有子项的总高度
+            totalHeight += listItem.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() *(listAdapter.getCount() - 1));
+        // listView.getDividerHeight()获取子项间分隔符占用的高度
+        // params.height最后得到整个ListView完整显示需要的高度
+        listView.setLayoutParams(params);
+    }
+
 }
